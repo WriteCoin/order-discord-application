aba1335f1e62d53596fd57959c32fc81c6dbcc03
WriteCoin <sovtsardom@gmail.com>
2025-01-06 00:07:22 +0300

    added file utils

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 import type { Client, Message, User } from "discord.js";
 import fs from "fs"
-import type { DateTime } from "luxon";
+import path from "path"
 
 export const isOwnBot = (user: User, client: Client) => client.user && client.user.id === user.id
 
@@ -9,23 +9,38 @@ export enum RolesTextDisable {
     Бестекстовый
 }
 
-export const saveMessages = (messages: Message[]) => {
+export const createFileIfNotExists = (filePath: string, data: string = '') => {
+    const dir = path.dirname(filePath)
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+    
+    // Создаем файл, если он не существует
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, data);
+    }
+}
+
+export const saveMessages = (messages: Message[], file: string = "./data/deletedMessages.json") => {
     console.log("Сохранение сообщений")
     if (messages.length <= 0) {
         return
     }
-    const deletedMessagesData = fs.readFileSync("./data/deletedMessages.json", "utf-8")
+    createFileIfNotExists(file, "[]")
+    const deletedMessagesData = fs.readFileSync(file, "utf-8")
     const deletedMessages = JSON.parse(deletedMessagesData || "[]")
     const newDeletedMessages = [...deletedMessages, ...messages]
-    fs.writeFileSync("./data/deletedMessages.json", JSON.stringify(newDeletedMessages), 'utf-8')
+    fs.writeFileSync(file, JSON.stringify(newDeletedMessages), 'utf-8')
 }
 
 export interface BotData {
     exitDate: string
 }
 
-export const saveBotData = (data: BotData) => {
-    const lastData = JSON.parse(fs.readFileSync("./data/bot.json", 'utf-8') || "{}")
+export const saveBotData = (data: BotData, file: string = "./data/bot.json") => {
+    createFileIfNotExists(file, "{}")
+    const lastData = JSON.parse(fs.readFileSync(file, 'utf-8') || "{}")
     const newData = Object.assign(lastData, data)
-    fs.writeFileSync("./data/bot.json", JSON.stringify(newData), 'utf-8')
+    fs.writeFileSync(file, JSON.stringify(newData), 'utf-8')
 }
